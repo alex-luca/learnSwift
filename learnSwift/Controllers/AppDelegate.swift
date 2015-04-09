bd4a29523104cfd7c9e73d30a2832db4bde29673
Alexandra Luca <aluca@thoughtworks.com>
2015-04-09 10:36:06 +1000

    initial commit

---
+++ learnSwift/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  learnSwift
+//
+//  Created by Alexandra Luca on 4/9/15
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
